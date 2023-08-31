2afedacfb80b76a4d92a56c2c27388aa0f7539c2
Qiitoboy <ransfordtakyi149@gmail.com>
2023-08-31 09:04:48 +0000

    Add files via upload

---
+++ Login server/login_server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, redirect, url_for
+app =Flask(__name__)
+
+
+users_database = {
+    "Ransford" : "August52003.",
+    "Randy" : "Qiitoboykingjr"
+}
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username in users_database and users_database[username] == password:
+            return render_template("homepage.html")
+        else:
+            return render_template("errorpage.html")
+
+
+    return render_template("login.html")
+
+#@app.route("/success")
+#def success():
+ #   return "Login successful!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
